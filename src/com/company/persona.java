da6c539d731e32f58a8a2b52a6d79486b73cde8d
Joan Piza Ferra <pizajoan97@gmail.com>
2016-11-23 17:52:02 +0100

    Commit inicial classe persona

---
+++ src/com/company/persona.java
@@ -0,0 +1,35 @@
+package com.company;
+
+/**
+ * Created by jpizaf on 23/11/16.
+ */
+public class persona {
+
+    private String dni;
+    private String nom;
+    private String cognom;
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCognom() {
+        return cognom;
+    }
+
+    public void setCognom(String cognom) {
+        this.cognom = cognom;
+    }
+}
